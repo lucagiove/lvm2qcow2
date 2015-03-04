c5c1079c9695fb7007312eef565783fbe0edcb05
Luca Giovenzana <luca@giovenzana.org>
2015-03-04 01:32:20 +0100

    [FEATURE] first draft of Device.__init__()

--- lvm2qcow2.py
+++ lvm2qcow2.py
@@ -16,6 +16,8 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
+import subprocess
 from argparse import ArgumentParser
 
 __author__ = "Luca Giovenzana <luca@giovenzana.org>"
@@ -23,17 +25,49 @@ __date__ = "2015-02-28"
 __version__ = "0.1dev"
 
 
+class Device():
+    def __init__(self, path):
+        # TODO test exists
+        # TODO test is a block device
+        # TODO consider try catch
+        # get path, vg, lv and size from lvdisplay
+        out = subprocess.check_output(['sudo', 'lvdisplay', path])
+        lv_display = re.findall('LV Path\s+(.+)\s+'
+                                'LV Name\s+(.+)\s+'
+                                'VG Name\s+(.+)', out)
+        lv_size = re.findall('LV Size\s+(.+)', out)
+        self.path = lv_display[0][0]
+        self.lv = lv_display[0][1]
+        self.vg = lv_display[0][2]
+        self.size = lv_size[0]
+
+    def create_snapshot(self, name, size):
+        # TODO test whether there is enough space
+        pass
+        #
+
+    def list_snapshot(self, pattern):
+        pass
+
+    def delete_snapshot(self, name):
+        # check if not used
+        pass
+        # delete the snapshot
+
+
 def main():
 
     parser = ArgumentParser()
-    parser.add_argument("-g", "--vg",
-                        action='store', dest='VOLUMEGROUP',
-                        help="source volume group where the logical volume is "
-                              "stored", required=True)
-    parser.add_argument("-v", "--lv",
-                        action='store', dest='LOGICALVOLUME',
-                        help="source logical volume that you want to snapshot "
-                             "and backup", required=True)
+    parser.add_argument("-s", "--source",
+                        action='store', dest='SOURCE',
+                        help="source logical volume device that you want to "
+                             "snapshot and backup", required=True)
+
+    parser.add_argument("-d", "--destination",
+                        action='store', dest='DESTINATION',
+                        help="destination path where the script saves "
+                             "the qcow2", required=True)
+
     parser.add_argument("-q", "--qcow2",
                         action='store', dest='IMAGE',
                         help="destination filename for the backup qcow2 image")
@@ -41,12 +75,15 @@ def main():
     parser.add_argument("-s", "--snapshot-size",
                         action='store', dest='SIZE', type=int,
                         help="")
+
     parser.add_argument('--version', action='version',
                         version="%(prog)s {}".format(__version__))
 
     args = parser.parse_args()
 
-    # Check input
+    # Check vg
+    # Check lv
+    # Check destination isdir
     # Check pending snapshots
     # Check space left in the vg
     # Snapshot lv
