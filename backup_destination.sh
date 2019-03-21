0720b57b8be9c693e7e9fd341c8daa1946cc03c7
Luca Giovenzana <luca@giovenzana.org>
2019-03-22 00:15:56 +0100

    Added a script to verify which between two hdd/path is available

---
+++ backup_destination.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Source this file to verify which path between two is 
+# available for storing data.
+
+DST_1=/mnt/usb_backup_6tb/snapshots
+DST_2=/mnt/usb_backup_6tb_2/snapshots
+
+if [[ -d "$DST_1" ]]; then
+	DST_AVAIL=$DST_1
+elif [[ -d "$DST_2" ]]; then
+	DST_AVAIL=$DST_2
+else
+	echo "ERROR: Cannot find backup destination"
+fi
